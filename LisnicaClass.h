c143db5c7658bc75b6007f201c2c546aeba60298
Marko Šeparović <maseparov@vsite.hr>
2024-12-14 10:02:42 +0100

    U LisnicaClass.h dodao klasu LisnicaClass i sto ce sto vratiti

--- LisnicaClass.h
+++ LisnicaClass.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <iostream>
+#include "VrijednosniPapir.h"
 using namespace std;
 namespace markot4 {
 	class LisnicaClass
 	{
-
+		void dodajVrijednosniPapir(VrijednosniPapir vp);
+		void izbaciVrijednosniPapir(string oznaka);
+		int promjenaKolicine(int promjena, string oznaka); // vrati novu kolicinu
 	};
 }
 
