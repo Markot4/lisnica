075d67246070e05b31f5a0ffaa1b2372e984bef6
Marko Šeparović <maseparov@vsite.hr>
2025-02-08 18:38:44 +0100

    obrisao  generirane komentare (//Tips for Getting Started) i zakomentirani nekorišteni kod u Lisnica.cpp

--- Lisnica.cpp
+++ Lisnica.cpp
@@ -11,15 +11,6 @@
 using namespace markot4;
 using namespace std;
 
-/*int main()
-{
-    LisnicaClass lisnica;
-
-    VrijednosniPapir vp = VrijednosniPapir("ATl-A-R");
-    cout << vp.oznaka << endl;
-
-}
-*/
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         cout << "Nedovoljno argumenata!" << endl;
@@ -81,15 +72,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
